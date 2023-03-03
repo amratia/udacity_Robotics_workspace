14f7a1d57476a27772d4fbbf4819c8a48c894a83
amratia <amratia@aucegypt.edu>
2023-03-03 18:41:56 +0200

    initial commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /home/amratia/workspace/catkin_ws/src
+# Build directory: /home/amratia/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
+subdirs("simple_arm")
+subdirs("turtlebot_simulator/turtlebot_gazebo")
+subdirs("turtlebot_simulator/turtlebot_simulator")
+subdirs("turtlebot_simulator/turtlebot_stage")
+subdirs("turtlebot_simulator/turtlebot_stdr")
+subdirs("ball_chaser")
---
+++ firstProject/README.md
@@ -0,0 +1,6 @@
+# Gazebo-Build-My-World
+This project is part of a udacity nanodegree in Robotics Software Engineering.
+It was required to design a model and include it in a Gazebo world
+![WhatsApp Image 2023-01-21 at 16 01 14](https://user-images.githubusercontent.com/90156505/213871472-a176c4a8-9361-474a-a200-aa667952a868.jpg)
+
+A Plugin is used to display a welcome message when launching the Gazebo world. The C++ script is included in the script folder and is run by the CMakeLists.txt
